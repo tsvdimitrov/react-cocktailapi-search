71b5cb87ac4c28e4d73c9ca67ac884c68e9171ee
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-05-21 10:33:46 +0300

    Create About.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus,
+				dicta fugit molestias hic reprehenderit enim, repellendus tempore a,
+				natus quibusdam est facilis pariatur! Quod molestiae libero, harum
+				soluta commodi autem corporis illo maiores. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Id nisi omnis error.{" "}
+			</p>
+		</section>
+	);
+};
+
+export default About;
